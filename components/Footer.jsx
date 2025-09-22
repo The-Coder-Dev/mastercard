1f5b1b59af9a73c952d25dee49a0df69136572c1
Dev Sharma <devsharma2057@gmail.com>
2025-09-22 22:49:23 +0530

    Footer Updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,6 @@ const Footer = () => {
     <div className="bg-[#141413] w-full pt-10 pb-5 flex items-center justify-center flex-col gap-4">
         <Image src="/Logo.png" width={60} height={50} alt=""/>
         <h1 className="text-[#817e7a] text-6xl md:text-9xl font-bold ">MASTERCARD</h1>
-        <p className="uppercase text-zinc-700">- made by Dev -</p>
     </div>
   );
 };

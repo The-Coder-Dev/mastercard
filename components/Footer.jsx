c3d384eaaff7b9dc1ffb0ef6eee0c94f60bfedfa
Dev Sharma <devsharma2057@gmail.com>
2025-09-22 23:20:59 +0530

    Footer and Hero Updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="bg-[#141413] w-full pt-10 pb-5 flex items-center justify-center flex-col gap-4">
+    <div className="bg-[#141413] w-full pt-10 pb-10 flex items-center justify-center flex-col gap-4">
         <Image src="/Logo.png" width={60} height={50} alt=""/>
-        <h1 className="text-[#817e7a] text-6xl md:text-9xl font-bold ">MASTERCARD</h1>
+        <h1 className="text-[#817e7a] text-5xl md:text-9xl font-bold ">MASTERCARD</h1>
     </div>
   );
 };
